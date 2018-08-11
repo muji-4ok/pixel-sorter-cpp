6c7e9ead2afe503ee4e1b305cb7b3fe2a74170af
muji-4ok <33330656+muji4ok@users.noreply.github.com>
2018-08-12 02:22:10 +0300

    Java support

--- PixelSorterVCDLL/PixelSorterVCDLL.h
+++ PixelSorterVCDLL/PixelSorterVCDLL.h
@@ -13,3 +13,30 @@ extern "C" PIXELSORTERVCDLL_API const unsigned char *sortImage(unsigned int widt
                                                                const char *pathType, int maxIntervals, bool randomizeIntervals,
                                                                int angle, bool toMerge, bool toReverse, bool toMirror,
                                                                bool toInterval, int lowThreshold);
+#include <jni.h>
+
+#ifndef _Included_pixelsorter_SortLib
+#define _Included_pixelsorter_SortLib
+#ifdef __cplusplus
+extern "C" {
+    #endif
+    /*
+    * Class:     pixelsorter_SortLib
+    * Method:    multiplyBy5
+    * Signature: (I)I
+    */
+    JNIEXPORT jint JNICALL Java_pixelsorter_SortLib_multiplyBy5
+    (JNIEnv *, jobject, jint);
+
+    /*
+    * Class:     pixelsorter_SortLib
+    * Method:    sortImage
+    * Signature: (II[BLjava/lang/String;IZIZZZZI)[B
+    */
+    JNIEXPORT jbyteArray JNICALL Java_pixelsorter_SortLib_sortImage
+    (JNIEnv *, jobject, jint, jint, jbyteArray, jstring, jint, jboolean, jint, jboolean, jboolean, jboolean, jboolean, jint);
+
+    #ifdef __cplusplus
+}
+#endif
+#endif
